b2dfef7a8d4e40de90b1e1fab677b5d74945921e
sergtk <sergtk@users.noreply.github.com>
2021-06-04 13:00:30 +0300

    Update 0044_Wildcard_Matching.cs
    Reduce memory usage

--- 0044_Wildcard_Matching.cs
+++ 0044_Wildcard_Matching.cs
@@ -28,28 +28,29 @@ public class Solution {
     public bool IsMatch(string s, string p) {
         int ns = s.Length;
         int np = p.Length;
-        bool[,] mem = new bool[ns+1, np+1];
+        bool[] mem1 = new bool[np+1];
+        bool[] mem = new bool[np+1];
         
-        mem[0, 0] = true;
+        mem[0] = true;
         for (int pi = 1; pi <= np; pi++) {
-            mem[0, pi] = p[pi-1] == '*' && mem[0, pi-1];
-            //System.Console.WriteLine($"mem[0, {pi}]={mem[0, pi]}");
+            mem[pi] = p[pi-1] == '*' && mem[pi-1];
         }
         for (int si = 1; si <= ns; si++) {
+            (mem, mem1) = (mem1, mem);
+            mem[0] = false;
             for (int pi = 1; pi <= np; pi++) {
                 bool v;
                 if (p[pi-1] == '?') {
-                    v = mem[si-1, pi-1];
+                    v = mem1[pi-1];
                 } else if (p[pi-1] == '*') {
-                    v = mem[si-1, pi] || mem[si, pi-1];
+                    v = mem1[pi] || mem[pi-1];
                 } else {
-                    v = p[pi-1] == s[si-1] && mem[si-1, pi-1];
+                    v = p[pi-1] == s[si-1] && mem1[pi-1];
                 }
-                mem[si, pi] = v;
+                mem[pi] = v;
             }
         }
-        //Console.WriteLine(mem[3, 1]);
-        bool ret = mem[ns, np];
+        bool ret = mem[np];
         return ret;
     }
 }
